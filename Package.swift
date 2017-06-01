42af05bafb802741554bbe991b1ed0f4a0927419
Jack <xuwenjiejack@gmail.com>
2017-06-01 23:06:38 +0800

    Add Package

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  PageKit
+//
+//  Created by Jack on 4/28/17.
+//
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "PageKit",
+    dependencies : [],
+    exclude: ["Demo", "Dragram", "Tests"]
+)
