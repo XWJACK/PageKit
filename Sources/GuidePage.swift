dce82599661888b5066b4364310f21e42c4f5247
Jack <xuwenjiejack@gmail.com>
2017-06-11 11:02:03 +0800

    Add guide page to project

---
+++ Sources/GuidePage.swift
@@ -0,0 +1,12 @@
+//
+//  GuidePage.swift
+//  PageKit
+//
+//  Created by Jack on 6/10/17.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+import UIKit
+
+open class GuidePage: ReuseContainer {
+}
